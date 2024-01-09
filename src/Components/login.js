de140c188d0511c21d184cbfaa971620599c6eeb
Philippe Prevost <philipou1802@gmail.com>
2024-01-09 16:50:39 +0100

    working on login

--- src/Components/login.js
+++ src/Components/login.js
@@ -1,18 +1,32 @@
+import { useState } from "react";
+import axios from "axios";
 import App from "../App";
 
+
 const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    // check if user exist in db
+    const checkUser = (users) => {
+        const user = users.find(
+            (user) => user.email === email && user.password === password);
+            console.log(user)
+            if (user.email === email && user.password === password) return user;
+      };
+
+      const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if (email === "" || password === "") {
+            alert("All fields are required");
+        }
+
+        
+      }
+
     return (
         <div className='container'>
         <form className='form-container' onSubmit={handleSubmit}>
-          <h1>Register User</h1>
-          <label>
-            <input
-              type='text'
-              placeholder='Name'
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-          </label>
           <label>
             <input
               type='text'
@@ -30,7 +44,7 @@ const Login = () => {
             />
           </label>
           <button className='btn' type="submit">
-            Register
+            <p>Log in</p>
           </button>
           {error && <p className="error-message">{error}</p>}
         </form>
